75953c63e776734b020c1f631f3204aa46b3afc7
Gil <0473386@students.dadeschools.net>
2018-04-18 11:33:52 -0400

    ghewjhv

---
+++ Trivia-Game-master/logic.js
@@ -0,0 +1,22 @@
+var panel = $(" ");
+var countStartNumber = 30;
+
+//Question set
+var questions = [{
+	question: " _________? ", 
+	answers: ["____", "____", "_____", "____"],
+	correctAnswer: "_____",
+	image: "______"
+
+},{
+	question: " ______ ", 
+	answers: ["____", "_____", "_____", "_____"],
+	correctAnswer: "______",
+	image: "______"
+}
+
+
+
+
+
+]
